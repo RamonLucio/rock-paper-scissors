f2c990ec191c36b8861a51e8d1e95fc6e7d8cd52
Ramon <ramonluciolinhares@gmail.com>
2020-07-20 17:47:38 -0300

    Aprimoramento da função playRound

--- script.js
+++ script.js
@@ -10,19 +10,25 @@ function computerPlay(){
         return 'scissors'
     }
     else {
-        return 'Something went wrong!'
+        return 'Something went wrong! Please try again'
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection == 'rock' &&  computerSelection == 'scissors'){
-        return 'You won! Rock beats Scissors'
+	if (playerSelection == 'rock' &&  computerSelection == 'scissors' || playerSelection == 'paper' &&  computerSelection == 'rock' || playerSelection == 'scissors' &&  computerSelection == 'paper'){
+        return 'You won!'
     }
-    else if (playerSelection == 'rock' &&  computerSelection == 'paper'){
-        return 'You lose! Paper beats Rock'
+    else if (playerSelection == 'rock' &&  computerSelection == 'paper' || playerSelection == 'paper' &&  computerSelection == 'scissors' || playerSelection == 'scissors' &&  computerSelection == 'rock') {
+        return 'You lose!'
+    }
+    else if (playerSelection == computerSelection) {
+        return 'Draw!'
+    }
+    else {
+        return 'Something went wrong! Please try again'
     }
 }
 
-const playerSelection = 'rock'
+const playerSelection = prompt('Type Rock, Paper or Scissors:').toLowerCase()
 const computerSelection = computerPlay()
 console.log(playRound(playerSelection, computerSelection))
