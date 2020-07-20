95579fd9cde954fd74f0b1caa8de06536ca6acfb
Ramon <ramonluciolinhares@gmail.com>
2020-07-20 09:08:43 -0300

    Declaração da função playRound e chamada das funções computerPlay e playRound

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function computerPlay(){
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber == 0){
-        return 'ROCK'
+        return 'rock'
     }
     else if (randomNumber == 1){
-        return 'PAPER'
+        return 'paper'
     }
     else if (randomNumber == 2) {
-        return 'SCISSORS'
+        return 'scissors'
     }
     else {
         return 'Something went wrong!'
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection == 'rock' &&  computerSelection == 'scissors'){
+        return 'You won! Rock beats Scissors'
+    }
+    else if (playerSelection == 'rock' &&  computerSelection == 'paper'){
+        return 'You lose! Paper beats Rock'
+    }
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
