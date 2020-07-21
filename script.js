ef517f4591a3ca989db105c501efc008d2299ef4
Ramon <ramonluciolinhares@gmail.com>
2020-07-21 20:39:37 -0300

    Create buttons and Add an event listener to them

--- script.js
+++ script.js
@@ -15,11 +15,15 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection == 'rock' &&  computerSelection == 'scissors' || playerSelection == 'paper' &&  computerSelection == 'rock' || playerSelection == 'scissors' &&  computerSelection == 'paper') {
+    if (playerSelection == 'rock' &&  computerSelection == 'scissors' ||
+    playerSelection == 'paper' &&  computerSelection == 'rock' ||
+    playerSelection == 'scissors' &&  computerSelection == 'paper') {
         playerScore++;
         return 'You won!'
     }
-    else if (playerSelection == 'rock' &&  computerSelection == 'paper' || playerSelection == 'paper' &&  computerSelection == 'scissors' || playerSelection == 'scissors' &&  computerSelection == 'rock') {
+    else if (playerSelection == 'rock' &&  computerSelection == 'paper' ||
+    playerSelection == 'paper' &&  computerSelection == 'scissors' ||
+    playerSelection == 'scissors' &&  computerSelection == 'rock') {
         computerScore++;
         return 'You lose!'
     }
@@ -31,16 +35,25 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        const playerSelection = prompt('Type Rock, Paper or Scissors:').toLowerCase()
-        const computerSelection = computerPlay()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    console.log(`Your score: ${playerScore}\nComputer score: ${computerScore}`)
-}
+// function game() {
+//     for (i = 0; i < 5; i++) {
+//         const playerSelection = prompt('Type Rock, Paper or Scissors:').toLowerCase()
+//         const computerSelection = computerPlay()
+//         console.log(playRound(playerSelection, computerSelection))
+//     }
+//     console.log(`Your score: ${playerScore}\nComputer score: ${computerScore}`)
+// }
+let playerScore = 0;
+let computerScore = 0;
 
-var playerScore = 0;
-var computerScore = 0;
+// game()
 
-game()
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.id;
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    });
+  });
+  
